009f3e345360cb96204ab1071aeca4c19eb7fccc
brandondelpozo <branzzel@gmail.com>
2021-11-11 09:02:17 -0500

    update grammar

---
+++ grammar/grammar.js
@@ -0,0 +1,24 @@
+// --Declaring variables--
+// const
+const firstName = "Jean-Luc"
+const lasName = "Picard"
+
+const PI = 3.14159
+const API_KEY = 'cnview8773jass'
+
+// let
+let ship = "Stargazer"
+ship = "Enterprise" // ship now equals "Enterprise"
+
+// var, it was deprecated after ES6, but it's good to know it.
+
+// --Data types--
+// typeof and equality
+typeof(1) // returns "number"
+typeof("hello") // returns "string"
+
+// loose equality and strict equality
+let myAge = 28
+const age = "38"
+myAge == age // this return false
+myAge === age // this return true
