a55d632ec269bd76e2c198ceb59065c2416f815a
brandondelpozo <branzzel@gmail.com>
2022-12-14 22:03:56 -0500

    create new example

--- mosh_yt/index.js
+++ mosh_yt/index.js
@@ -1,1 +1,39 @@
-console.log('Hello World');
+/*
+let name = 'Mosh';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Mosh'; 
+let lastName = 'Hamedani';
+*/
+/* // Const doesn't allowed to change its value and should be the default keyword
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate)
+*/
+
+/*
+let name = 'Mosh';  // String Literal
+let age = 30;  // Number Literal
+let isApproved = true; // Boolean
+let firstName = undefined;
+let selectedColor = null;
+*/
+
+
+let person = {
+    name: 'Mosh',
+    age: 30
+}
+
+console.log(person)
+
+
+
+
+
